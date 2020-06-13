46cddcd40610b602d7d55fd14758f874720ee7c2
Robert Clabough <rclabough@launchcg.com>
2020-06-13 10:49:56 -0700

    jQuery init

--- src/js/sizing-calculator.js
+++ src/js/sizing-calculator.js
@@ -3,3 +3,28 @@ if (!$) {
     throw new Error(`jQuery not initialized or found!`);
 }
 
+$(document).ready(() => {
+    var isFitted = false;
+    var height = 0;
+    var chest = 0;
+    var waist = 0;
+    var seat = 0;
+    var wearingLayers = false;
+    var lowCrotch = false;
+    var muscularThighs = false;
+
+
+    /** Events */
+
+
+    /** Logic */
+    function calculate() {
+
+    }
+
+
+
+    /** Actions */
+
+
+})
