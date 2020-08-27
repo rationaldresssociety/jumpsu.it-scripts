d38617c6342217a5b27f4a9b7d2a41d269a9199d
Robert Clabough <rclabough@launchcg.com>
2020-08-27 13:45:38 -0700

    Adding more info into debug statements

--- src/js/table-generator.js
+++ src/js/table-generator.js
@@ -49,12 +49,12 @@ $(window).on('load', () => {
                         if (foundPattern.link) {
                             html += `<a href="${foundPattern.link}" target="_blank">${foundPattern.name}</a>`;
                         } else {
-                            console.warn(`WARN: Pattern ${foundPattern.name} did not have a link associated with it`);
+                            console.warn(`WARN: Body ${bodyType} FittedType: ${fittedtype} Pattern ${foundPattern.name} did not have a link associated with it`);
                             html += foundPattern.name
                         }
                         
                     } else {
-                        console.warn(`WARN: There was no pattern found for Height (${h.min} - ${h.max}) and Seat (${headers2[i]})`);
+                        console.warn(`WARN: Body ${bodyType} FittedType: ${fittedtype}  There was no pattern found for Height (${h.min} - ${h.max}) and Seat (${headers2[i].min} - ${headers2[i].max})`);
                     }
                     html += `</td>`
                 }
