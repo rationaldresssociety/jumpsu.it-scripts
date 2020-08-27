fd8f708682f8fe47b79151b8939ffbf1225a4641
Robert Clabough <rclabough@launchcg.com>
2020-08-27 13:48:05 -0700

    Misstyped fittedType

--- dist/table-generator.js
+++ dist/table-generator.js
@@ -10514,11 +10514,11 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 								if (foundPattern.link) {
 									html += "<a href=\"" + foundPattern.link + "\" target=\"_blank\">" + foundPattern.name + "</a>";
 								} else {
-									console.warn("WARN: Body " + bodyType + " FittedType: " + fittedtype + " Pattern " + foundPattern.name + " did not have a link associated with it");
+									console.warn("WARN: Body " + bodyType + " FittedType: " + fittedType + " Pattern " + foundPattern.name + " did not have a link associated with it");
 									html += foundPattern.name;
 								}
 							} else {
-								console.warn("WARN: Body " + bodyType + " FittedType: " + fittedtype + "  There was no pattern found for Height (" + h.min + " - " + h.max + ") and Seat (" + headers2[i].min + " - " + headers2[i].max + ")");
+								console.warn("WARN: Body " + bodyType + " FittedType: " + fittedType + "  There was no pattern found for Height (" + h.min + " - " + h.max + ") and Seat (" + headers2[i].min + " - " + headers2[i].max + ")");
 							}
 							html += "</td>";
 						}
